5767228fbd36b5fd56004a0418213ba858bafa10
MarcoSantino <marco.santino94@gmail.com>
2021-03-15 20:52:15 +0100

    add .env file

--- src/utils/interceptor.ts
+++ src/utils/interceptor.ts
@@ -1,9 +1,13 @@
 export async function interceptor(url: string, method = 'GET'): Promise<any> {
+    const {
+        REACT_APP_RAPIDAPI_KEY,
+        REACT_APP_RAPIDAPI_HOST
+    } = process.env;
     const response = await fetch(url, {
         method,
         headers: {
-            "x-rapidapi-key": '1e33b37031msh113802dd83ed5d5p1c5a21jsnb93136ed6aef',
-            "x-rapidapi-host": 'api-nba-v1.p.rapidapi.com'
+            "x-rapidapi-key": REACT_APP_RAPIDAPI_KEY as string,
+            "x-rapidapi-host": REACT_APP_RAPIDAPI_HOST as string
         }
     })
 
